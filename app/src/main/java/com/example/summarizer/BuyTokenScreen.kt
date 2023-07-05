a04e4f8dda2da2e0396c5a0d06692c8847e62a98
labmember003 <93467641+labmember003@users.noreply.github.com>
2023-07-05 20:17:46 +0530

    working buytokenscreen

--- app/src/main/java/com/example/summarizer/BuyTokenScreen.kt
+++ app/src/main/java/com/example/summarizer/BuyTokenScreen.kt
@@ -1,44 +1,139 @@
 package com.example.summarizer
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun BuyTokenScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Heading()
+        Features()
+
+
+    }
+
+}
 
 @Preview(showBackground = true)
 @Composable
-fun BuyTokenScreen() {
-    Column {
-        Row(
+fun ColumnScope.Features() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .weight(1f)
+    ) {
+        Text(
+            text = "Unlock Unlimited Access",
+            style = MaterialTheme.typography.body1.copy(
+                fontSize = 24.sp
+            ),
+        )
+        Feature(R.drawable.chatbot, "Powered By GPT - 4", "Latest ChatGPT AI Model")
+        Feature(R.drawable.speedometerr, "Higher Word Limits", "Type Longer Messages")
+        Feature(R.drawable.ad, "No Ads", "Enjoy Summarizer without ads")
+    }
+}
+
+@Composable
+fun Feature(imageId: Int, text: String, extraText: String) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .padding(6.dp)
+    ) {
+        Image(
+            painter = painterResource(id = imageId),
+            contentDescription = text,
+            contentScale = ContentScale.FillBounds,
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
+        )
+        Column(
+            modifier = Modifier
+                .padding(4.dp, 0.dp)
         ) {
             Text(
-                text = "Buy Token",
-                style = MaterialTheme.typography.subtitle1,
+                text = text,
+                style = MaterialTheme.typography.body1.copy(
+
+                ),
+                fontSize = 24.sp
             )
+            Text(
+                text = extraText,
+                fontSize = 16.sp
+            )
+        }
+    }
+}
+
+@Composable
+private fun Heading() {
+    Box(
+        modifier = Modifier
+            .padding(24.dp)
+            .fillMaxWidth(1f)
+    ) {
+        Text(
+            text = "Buy Token",
+            style = MaterialTheme.typography.body1.copy(
+                fontWeight = FontWeight.Bold
+            ),
+            fontSize = 24.sp,
+            modifier = Modifier
+                .align(Alignment.Center)
+        )
+        Box(
+            modifier = Modifier
+                .align(Alignment.CenterEnd)
+                .background(Color.LightGray, shape = CircleShape)
+                .clickable(onClick = {
+//                        TODO()
+                }),
+            contentAlignment = Alignment.Center
+        ) {
             Icon(
                 Icons.Filled.Close,
                 contentDescription = "Close",
                 modifier = Modifier
+
                     .clickable {
 //                        onDismiss()
                     }
             )
         }
     }
+}
+
+@Composable
+fun Happy() {
 
 }
--- app/src/main/java/com/example/summarizer/MainScreen.kt
+++ app/src/main/java/com/example/summarizer/MainScreen.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterialApi::class)
+
 package com.example.summarizer
 
 import android.content.ContentValues.TAG
@@ -17,24 +19,29 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ModalBottomSheetLayout
+import androidx.compose.material.ModalBottomSheetState
+import androidx.compose.material.ModalBottomSheetValue
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.rememberModalBottomSheetState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -54,18 +61,66 @@ import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import com.tom_roush.pdfbox.android.PDFBoxResourceLoader
 import com.tom_roush.pdfbox.pdmodel.PDDocument
 import com.tom_roush.pdfbox.text.PDFTextStripper
+import kotlinx.coroutines.launch
 import java.io.IOException
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterialApi::class)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun MainScreen() {
-    val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
-    val bottomSheetOpen = remember{ mutableStateOf(false) }
     val context = LocalContext.current
+    val modalSheetState = rememberModalBottomSheetState(
+        initialValue = ModalBottomSheetValue.Hidden,
+        confirmValueChange = { it != ModalBottomSheetValue.HalfExpanded },
+        skipHalfExpanded = true
+    )
+    val recognizer = remember {
+        TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+    }
+    val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri ->
+        if (uri != null) {
+            val text = extractTextFromPdf(uri, context)
+            Log.i("hapyhapyhapy", "text")
+            Log.i("hapyhapyhapy", text)
+        }
+    }
+    val launcher2 = rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri ->
+        uri?.let {
+            val bitmap: Bitmap = uriToBitmap(context, it)
+            getTextFromImage(bitmap, recognizer, context)
+        }
+    }
+    ModalBottomSheetLayout(
+        sheetState = modalSheetState,
+        sheetShape = RoundedCornerShape(topStart = 12.dp, topEnd = 12.dp),
+        sheetContent = {
+            BottomSheet(
+                modalSheetState,
+                onPdfOptionClicked = {
+                    launcher.launch("application/pdf")
+                },
+                onImageOptionClicked = {
+                    launcher2.launch("image/*")
+                }
+            )
+        }
+    ) {
+        MainScreenContent(
+            modalSheetState = modalSheetState
+        )
+    }
+}
+
+@Composable
+private fun MainScreenContent(
+    modalSheetState: ModalBottomSheetState
+) {
+    val scope = rememberCoroutineScope()
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.padding(4.dp)
+        modifier = Modifier
+            .padding(4.dp)
     ) {
         Column(
             verticalArrangement = Arrangement.Center,
@@ -73,8 +128,7 @@ fun MainScreen() {
             modifier = Modifier
                 .weight(0.5f)
         ) {
-            Row(
-                horizontalArrangement = Arrangement.Center,
+            Box(
                 modifier = Modifier
                     .padding(top = 16.dp)
                     .fillMaxWidth(1f)
@@ -82,31 +136,21 @@ fun MainScreen() {
                 Text(
                     text = "Upload Image Or E-Book",
                     modifier = Modifier
+                        .align(Alignment.Center)
                 )
-                Row(
-                    horizontalArrangement = Arrangement.End,
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
+                Tokens(
+                    Modifier
+                        .align(Alignment.CenterEnd)
                         .clip(RoundedCornerShape(8.dp))
                         .background(colorResource(id = R.color.light_grey))
                         .padding(8.dp)
-                ) {
-                    Image(
-                        painter = painterResource(R.drawable.token),
-                        contentDescription = "Your Image",
-                        contentScale = ContentScale.Crop,
-                        modifier = Modifier
-                            .size(32.dp)
-                            .clickable {
-                                DataManager.currentPage.value = PAGES.BUYTOKEN
-                            }
-                    )
-                    Text(
-                        text = "100",
-                    )
-                }
+                )
             }
-            UploadAnimation(bottomSheetOpen)
+            UploadAnimation(
+                onClick = {
+                    scope.launch { modalSheetState.show() }
+                }
+            )
             LineWithText()
         }
         val content = remember { mutableStateOf("") }
@@ -119,36 +163,9 @@ fun MainScreen() {
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
-                .weight(0.5F)
-        )
-        val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri ->
-            if (uri != null) {
-                val text = extractTextFromPdf(uri, context)
-                Log.i("hapyhapyhapy", "text")
-                Log.i("hapyhapyhapy", text)
-            }
-        }
-        val launcher2 = rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri ->
-            uri?.let {
-                val bitmap: Bitmap = uriToBitmap(context, it)
-                getTextFromImage(bitmap, recognizer, context)
-            }
-        }
-
-
-        BottomSheet(
-
-            isOpen = bottomSheetOpen.value,
-            onDismiss = { bottomSheetOpen.value = false },
-            onPdfOptionClicked = {
-                launcher.launch("application/pdf")
-            },
-            onImageOptionClicked = {
-                launcher2.launch("image/*")
-            }
+                .weight(0.5f)
         )
     }
-
 }
 
 
@@ -165,77 +182,100 @@ fun uriToBitmap(context: Context, uri: Uri): Bitmap {
 }
 
 @Composable
-fun UploadAnimation(open: MutableState<Boolean>) {
+fun ColumnScope.UploadAnimation(
+    onClick: () -> Unit
+) {
     val composition by rememberLottieComposition(spec = LottieCompositionSpec.RawRes(R.raw.upload))
     com.airbnb.lottie.compose.LottieAnimation(
         composition = composition,
         iterations = LottieConstants.IterateForever,
         modifier = Modifier
-            .size(350.dp)
+            .weight(1f)
             .clickable {
-                open.value = true
+                onClick()
             }
     )
 
 }
 
 @Composable
+fun Tokens(modifier: Modifier) {
+    Row(
+        horizontalArrangement = Arrangement.End,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(R.drawable.token),
+            contentDescription = "Your Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(32.dp)
+                .clickable {
+                    DataManager.currentPage.value = PAGES.BUYTOKEN
+                }
+        )
+        Text(
+            text = "100",
+        )
+    }
+}
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
 fun BottomSheet(
-    isOpen: Boolean,
-    onDismiss: () -> Unit,
+    state: ModalBottomSheetState,
     onPdfOptionClicked: () -> Unit,
     onImageOptionClicked: () -> Unit
 ) {
-    if (isOpen) {
-        Column(
+    val scope = rememberCoroutineScope()
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+            .padding(16.dp, 24.dp, 16.dp, 16.dp)
+
+    ) {
+        Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .background(Color.White)
-                .padding(16.dp, 24.dp, 16.dp, 16.dp)
-
+                .padding(bottom = 48.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Row(
+            Text(
+                text = "Choose the file format:",
+                style = MaterialTheme.typography.subtitle1,
+            )
+            Icon(
+                Icons.Filled.Close,
+                contentDescription = "Close",
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 48.dp),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(
-                    text = "Choose the file format:",
-                    style = MaterialTheme.typography.subtitle1,
-                )
-                Icon(
-                    Icons.Filled.Close,
-                    contentDescription = "Close",
-                    modifier = Modifier
-                        .clickable {
-                            onDismiss()
-                        }
-                )
-            }
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.photo),
-                    contentDescription = "",
-                    modifier = Modifier
-                        .clickable {
-                            onImageOptionClicked()
-                            onDismiss()
-                        }
-                )
-                Image(
-                    painter = painterResource(id = R.drawable.pdffff),
-                    contentDescription = "",
-                    modifier = Modifier
-                        .clickable {
-                            onPdfOptionClicked()
-                            onDismiss()
-                        }
-                )
-            }
+                    .clickable {
+                        scope.launch { state.hide() }
+                    }
+            )
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.photo),
+                contentDescription = "",
+                modifier = Modifier
+                    .clickable {
+                        onImageOptionClicked()
+                        scope.launch { state.hide() }
+                    }
+            )
+            Image(
+                painter = painterResource(id = R.drawable.pdffff),
+                contentDescription = "",
+                modifier = Modifier
+                    .clickable {
+                        onPdfOptionClicked()
+                        scope.launch { state.hide() }
+                    }
+            )
         }
     }
 }
@@ -278,4 +318,3 @@ private fun getTextFromImage(bitmap: Bitmap, recognizer: TextRecognizer, context
             }
     }
 }
-
