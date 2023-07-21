965118716852823df25d5b944b4c7efbb5006fce
labmember003 <93467641+labmember003@users.noreply.github.com>
2023-07-21 14:56:38 +0530

    bug fixes

--- app/src/main/java/com/example/summarizer/SettingsPage.kt
+++ app/src/main/java/com/example/summarizer/SettingsPage.kt
@@ -1,25 +1,30 @@
 package com.example.summarizer
 
-import androidx.compose.foundation.background
+import android.content.Intent
+import android.net.Uri
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
-
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+
 @Preview
 @Composable
 fun SettingsScreen() {
-    var selectedFontSize by remember { mutableStateOf(16) }
-
+    val context = LocalContext.current
+    val openUrlLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) {}
     Scaffold(
         topBar = {
             TopAppBar(
@@ -31,10 +36,10 @@ fun SettingsScreen() {
             it.calculateBottomPadding()
             Column(
                 modifier = Modifier.padding(16.dp),
-                verticalArrangement = Arrangement.spacedBy(22.dp)
+                verticalArrangement = Arrangement.spacedBy(0.dp)
             ) {
-                PrivacyPolicySection()
-                ResourcesUsedSection()
+                PrivacyPolicySection(openUrlLauncher)
+                ResourcesUsedSection(openUrlLauncher)
             }
         }
     )
@@ -42,42 +47,42 @@ fun SettingsScreen() {
 
 
 
+
 @Composable
-fun PrivacyPolicySection() {
+fun PrivacyPolicySection(openUrlLauncher: ManagedActivityResultLauncher<Intent, ActivityResult>) {
     Text(
         text = "Privacy Policy",
-        fontSize = 20.sp,
-        style = MaterialTheme.typography.subtitle1,
+        fontSize = 18.sp,
+        style = MaterialTheme.typography.h5,
         modifier = Modifier
-            .fillMaxWidth()
             .clickable {
-                openPrivacyPolicy()
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse("https://sites.google.com/view/falcon-summarizer/home")
+                openUrlLauncher.launch(intent)
             }
+            .padding(0.dp, 10.dp)
+            .fillMaxWidth()
     )
 }
 
 @Composable
-fun ResourcesUsedSection() {
+fun ResourcesUsedSection(openUrlLauncher: ManagedActivityResultLauncher<Intent, ActivityResult>) {
     Text(
         text = "Resources Used",
-        fontSize = 20.sp,
-        style = MaterialTheme.typography.subtitle1,
+        fontSize = 18.sp,
+        style = MaterialTheme.typography.h5,
         modifier = Modifier
-            .fillMaxWidth()
             .clickable {
-                openResourcesUsed()
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse("https://sites.google.com/view/falcon-summarizer/resources-used")
+                openUrlLauncher.launch(intent)
             }
+            .padding(0.dp, 10.dp)
+            .fillMaxWidth()
+
     )
 }
 
-fun openPrivacyPolicy() {
-    // TODO: Replace the URL with your privacy policy link
-    val privacyPolicyUrl = "https://www.example.com/privacy-policy"
-    // Launch the default browser to open the privacy policy link
-    // Replace `context` with the appropriate Android context if needed
-    // context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(privacyPolicyUrl)))
-    // Note: This code will not work inside the Compose function directly, it's just a placeholder for the behavior.
-}
 
 fun openResourcesUsed() {
     // TODO: Replace the URL with your privacy policy link
