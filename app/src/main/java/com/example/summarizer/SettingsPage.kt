cea9227d446aa3b177ae4cef46ad9f62a53d2e50
labmember003 <93467641+labmember003@users.noreply.github.com>
2023-07-21 19:07:17 +0530

    added oss libraries activity

--- app/src/main/java/com/example/summarizer/SettingsPage.kt
+++ app/src/main/java/com/example/summarizer/SettingsPage.kt
@@ -1,7 +1,10 @@
 package com.example.summarizer
 
+import android.content.ActivityNotFoundException
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.widget.Toast
 import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.ActivityResult
@@ -16,7 +19,9 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
+import androidx.core.content.ContextCompat.startActivity
+import com.falcon.summarizer.R
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 
 @Preview
 @Composable
@@ -38,43 +43,55 @@ fun SettingsScreen() {
                 modifier = Modifier.padding(16.dp),
                 verticalArrangement = Arrangement.spacedBy(0.dp)
             ) {
-                PrivacyPolicySection(openUrlLauncher)
-                ResourcesUsedSection(openUrlLauncher)
+                SectionOpenLinks(openUrlLauncher, "https://sites.google.com/view/falcon-summarizer/home", "Privacy Policy")
+                SectionOpenLinks(openUrlLauncher, "https://sites.google.com/view/falcon-summarizer/resources-used", "Resources Used")
+                ContactUsSection(openUrlLauncher, context)
+                OpenOpenSourceLibraryActivity(openUrlLauncher, "Open Source Libraries")
+
             }
         }
     )
 }
 
-
-
-
 @Composable
-fun PrivacyPolicySection(openUrlLauncher: ManagedActivityResultLauncher<Intent, ActivityResult>) {
+fun ContactUsSection(
+    openUrlLauncher: ManagedActivityResultLauncher<Intent, ActivityResult>,
+    context: Context
+) {
     Text(
-        text = "Privacy Policy",
+        text = "Contact Us",
         fontSize = 18.sp,
         style = MaterialTheme.typography.h5,
         modifier = Modifier
             .clickable {
-                val intent = Intent(Intent.ACTION_VIEW)
-                intent.data = Uri.parse("https://sites.google.com/view/falcon-summarizer/home")
-                openUrlLauncher.launch(intent)
+                val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
+                    data = Uri.parse("mailto:") // only email apps should handle this
+                    putExtra(Intent.EXTRA_EMAIL, arrayOf("usarcompanion@gmail.com"))
+                    putExtra(Intent.EXTRA_SUBJECT, "Regarding App " + R.string.app_name)
+                }
+                try {
+                    openUrlLauncher.launch(emailIntent)
+                } catch (e: ActivityNotFoundException) {
+                    Toast.makeText(context, "No Mail App Found", Toast.LENGTH_SHORT).show()
+                }
             }
             .padding(0.dp, 10.dp)
             .fillMaxWidth()
     )
 }
 
+
+
 @Composable
-fun ResourcesUsedSection(openUrlLauncher: ManagedActivityResultLauncher<Intent, ActivityResult>) {
+fun SectionOpenLinks(openUrlLauncher: ManagedActivityResultLauncher<Intent, ActivityResult>, url: String, title: String) {
     Text(
-        text = "Resources Used",
+        text = title,
         fontSize = 18.sp,
         style = MaterialTheme.typography.h5,
         modifier = Modifier
             .clickable {
                 val intent = Intent(Intent.ACTION_VIEW)
-                intent.data = Uri.parse("https://sites.google.com/view/falcon-summarizer/resources-used")
+                intent.data = Uri.parse(url)
                 openUrlLauncher.launch(intent)
             }
             .padding(0.dp, 10.dp)
@@ -84,11 +101,18 @@ fun ResourcesUsedSection(openUrlLauncher: ManagedActivityResultLauncher<Intent, 
 }
 
 
-fun openResourcesUsed() {
-    // TODO: Replace the URL with your privacy policy link
-    val privacyPolicyUrl = "https://www.example.com/privacy-policy"
-    // Launch the default browser to open the privacy policy link
-    // Replace `context` with the appropriate Android context if needed
-    // context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(privacyPolicyUrl)))
-    // Note: This code will not work inside the Compose function directly, it's just a placeholder for the behavior.
+@Composable
+fun OpenOpenSourceLibraryActivity(openUrlLauncher: ManagedActivityResultLauncher<Intent, ActivityResult>, title: String) {
+    val context = LocalContext.current
+    Text(
+        text = title,
+        fontSize = 18.sp,
+        style = MaterialTheme.typography.h5,
+        modifier = Modifier
+            .clickable {
+                startActivity(context, Intent(context, OssLicensesMenuActivity::class.java), null)
+            }
+            .padding(0.dp, 10.dp)
+            .fillMaxWidth()
+    )
 }
